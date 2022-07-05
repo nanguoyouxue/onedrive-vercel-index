dd5ab16653def5b94f9cfc03e2e0216863310edc
南国有雪 <47874078+nanguoyouxue@users.noreply.github.com>
2022-07-05 14:53:56 +0800

    Update FileListing.tsx

--- components/FileListing.tsx
+++ components/FileListing.tsx
@@ -331,7 +331,7 @@ const FileListing: FC<{ query?: ParsedUrlQuery }> = ({ query }) => {
       <>
         <Toaster />
 
-        {layout.name === 'List' ? <FolderListLayout {...folderProps} /> : <FolderGridLayout {...folderProps} />}
+        {layout.name === 'Grid' ? <FolderGridLayout {...folderProps} /> : <FolderListLayout {...folderProps} />}
 
         {!onlyOnePage && (
           <div className="rounded-b bg-white dark:bg-gray-900 dark:text-gray-100">
